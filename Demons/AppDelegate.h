8c56c15d16c846761adc92e688e78b3118a16937
蔡伦 <liuqin.sheng@alipay.com>
2014-06-12 14:42:11 +0800

    init

---
+++ Demons/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demons
+//
+//  Created by liuqin.sheng on 6/12/14.
+//  Copyright (c) 2014 peaches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demons/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Demons
+//
+//  Created by liuqin.sheng on 6/12/14.
+//  Copyright (c) 2014 peaches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
