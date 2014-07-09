1f3544fc01e9afcda4699ffc719cae61d5891053
蔡伦 <liuqin.sheng@alipay.com>
2014-07-09 14:40:20 +0800

    add presentation controller demo

---
+++ PresentationControllers/PresentationControllersTests/PresentationControllersTests.m
@@ -0,0 +1,34 @@
+//
+//  PresentationControllersTests.m
+//  PresentationControllersTests
+//
+//  Created by liuqin.sheng on 7/9/14.
+//  Copyright (c) 2014 liuqin.sheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PresentationControllersTests : XCTestCase
+
+@end
+
+@implementation PresentationControllersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
